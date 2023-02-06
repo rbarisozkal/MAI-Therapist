6e2313868ecc0787a8ca0092ceb5a10d2b8824eb
Renas Barış Özkal <barisozkal11@gmail.com>
2023-02-07 00:39:35 +0300

    emitting event bug

--- src/components/RegisterPageComponents/Register.vue
+++ src/components/RegisterPageComponents/Register.vue
@@ -1,18 +1,19 @@
 <template>
     <v-app>
         <v-form v-model="valid" ref="form" @submit.prevent="submit">
-            <v-text-field v-model="name" :rules="nameRules"  label="Name"></v-text-field>
+            <v-text-field v-model="name" :rules="nameRules" label="Name"></v-text-field>
 
-            <v-text-field v-model="surname" :rules="SurnameRules"  label="Surname"></v-text-field>
+            <v-text-field v-model="surname" :rules="SurnameRules" label="Surname"></v-text-field>
 
-            <v-text-field v-model="phone" :rules="[v => !!v || 'Phone Number is required']" :counter="11" label="Phone Number"></v-text-field>
+            <v-text-field v-model="phone" :rules="[v => !!v || 'Phone Number is required']" :counter="11"
+                label="Phone Number"></v-text-field>
 
             <v-text-field v-model="email" :rules="[v => !!v || 'Email is required']" label="E-mail"></v-text-field>
 
             <v-text-field v-model="password" :type="passwordFieldType" :rules="[v => !!v || 'Password is required']"
                 label="Password"></v-text-field>
-                <v-switch type="password"  v-model="showPassword" hide-details true-value="hide" false-value="show" :label="`${showPassword}`" @click="switcVisibility"
-  ></v-switch>
+            <v-switch type="password" v-model="showPassword" hide-details true-value="hide" false-value="show"
+                :label="`${showPassword}`" @click="switcVisibility"></v-switch>
             <v-checkbox :rules="[v => !!v || 'You must agree to continue!']" v-model="isTherapist" value="1"
                 label="I'm a therapist" type="checkbox"></v-checkbox>
 
@@ -23,16 +24,36 @@
     </v-app>
 </template>
 <style>
-.v-form{
+.v-form {
     width: 100%;
     height: auto;
     padding: 10rem 16rem 10rem 16rem;
 }
 </style>
-<script>
+<script setup>
 import { shallowEqual } from '@babel/types';
+import { Amplify, Auth } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-vue';
+import '@aws-amplify/ui-vue/styles.css';
+const services = {
+    async handleSignUp(formData) {
+        let { username, password, attributes } = formData;
+        // custom username
+        username = username.toLowerCase();
+        attributes.email = attributes.email.toLowerCase();
+        return Auth.signUp({
+            username,
+            password,
+            attributes,
+            autoSignIn: {
+                enabled: true,
+            },
+        });
+    },
+};
 
-export default {
+
+/*export default {
     data() {
         return {
             users: [],
@@ -56,46 +77,43 @@ export default {
     },
     methods: {
         submit() {
-            
         },
-         register() {
-            
-                const newUser = {
-                    name: this.name,
-                    surname: this.surname,
-                    phone: this.phone,
-                    email: this.email,
-                    password: this.password,
-                    isTherapist: this.isTherapist
-                };
-this.users.push(newUser);
-this.currentUser = newUser;
-this.$nextTick(function() {
-    this.$refs.name.focus();
-})
-; 
+        register() {
+            const newUser = {
+                name: this.name,
+                surname: this.surname,
+                phone: this.phone,
+                email: this.email,
+                password: this.password,
+                isTherapist: this.isTherapist
+            };
+            this.users.push(newUser);
+            this.currentUser = newUser;
+            this.$nextTick(function () {
+                this.$refs.name.focus();
+            });
 
         },
         switcVisibility() {
             this.passwordFieldType = this.passwordFieldType === "password" ? "text" : "password";
         },
     },
-    mounted(){
-       if(localStorage.users){
-        this.users = JSON.parse(localStorage.users);
-       }
+    mounted() {
+        if (localStorage.users) {
+            this.users = JSON.parse(localStorage.users);
+        }
     },
     watch: {
         users: {
             handler(newUser) {
-            localStorage.users = JSON.stringify(newUser);
-        },
-    deep: true
-}
+                localStorage.users = JSON.stringify(newUser);
+            },
+            deep: true
+        }
     },
 
 }
-
+*/
 </script>
 <style>
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,93 +1,26 @@
 <template>
   <div>
-    <authenticator :form-fields="formFields" :sign-up-attributes="signUpAttributes">
-      <template v-slot="{ user }">
-        <h1>Hello {{ user.username }}!</h1>
-        <button @click="signOut">Sign Out</button>
-      </template>
-    </authenticator>
+    <Login @userData="receiveUserData" />
+    <h1 v-if="userData">Hello akjsdh{{ userData.username }}!</h1>
+    {{ userData }}
   </div>
 </template>
-<script setup>
-import LoginForm from "../../src/components/LoginPageComponents/Login.vue"
-import { Authenticator } from "@aws-amplify/ui-vue"
-import "@aws-amplify/ui-vue/styles.css"
-import { useAuthenticator } from "@aws-amplify/ui-vue"
-import { Amplify } from "aws-amplify"
-import awsconfig from "@/aws-exports"
-import { Auth } from 'aws-amplify';
-Amplify.configure(awsconfig);
-const signUpAttributes = ['custom:credit_card']
-const user = useAuthenticator({
+<script>
+import Login from "../components/LoginPageComponents/Login.vue";
+export default {
+  name: "LoginView",
+  components: {
+    Login,
+  },
+  setup() {
+    const receiveUserData = (user) => {
+      this.userData = user
+    }
 
-});
-console.log(user);
-
-
-// export default {
-//   name: "LoginView",
-//   components: {
-//     LoginForm,
-//   },
-//   data() {
-//     return {
-//       user: null,
-//       signOut: null,
-//       services: {
-//         async handleSignUp(formData) {
-//           let { username, password, attributes } = formData;
-//           // custom username
-//           username = username.toLowerCase();
-//           attributes.email = attributes.email.toLowerCase();
-//           return Auth.signUp({
-//             username,
-//             password,
-//             attributes,
-//             autoSignIn: {
-//               enabled: true,
-//             },
-//           })
-//         },
-//       },
-//       formFields: {
-//         signIn: {
-//           username: {
-//             placeholder: 'Enter Your Email',
-//             isRequired: true,
-//             label: 'Email:'
-//           },
-//         },
-//         signUp: {
-//           username: {
-//             placeholder: 'Enter Your Email',
-//             isRequired: true,
-//             label: 'Email:'
-//           },
-//           password: {
-//             placeholder: 'Enter Your Password',
-//             isRequired: true,
-//             label: 'Password:'
-//           },
-//           name: {
-//             placeholder: 'Enter Your Name and Surname',
-//             isRequired: true,
-//             label: 'Name:'
-//           },
-
-//         }
-//       }
-//     }
-//   },
-//   methods: {
-
-//   },
-//   computed: {
-//     checkServices() {
-//       console.log(this.services)
-//     }
-//   },
-//   beforeCreate() {
-
-//   },
-
+    return {
+      userData: null,
+      receiveUserData
+    }
+  }
+};
 </script>
