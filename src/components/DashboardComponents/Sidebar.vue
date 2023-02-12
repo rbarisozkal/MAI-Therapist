b998469089804101ee49b67f2b927d0fd7ed9fa5
Renas Barış Özkal <barisozkal11@gmail.com>
2023-02-12 21:03:54 +0300

    dynamic components in dashboard added :: sidebar added

--- src/components/DashboardComponents/Sidebar.vue
+++ src/components/DashboardComponents/Sidebar.vue
@@ -1,25 +1,44 @@
 <template>
-
-    <v-card>
-        <v-layout>
-            <v-navigation-drawer permanent theme="dark">
-                <v-list nav>
-                    <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
-                    <v-list-item prepend-icon="mdi-account-supervisor-circle" title="My Patients"
-                        value="myPatients"></v-list-item>
-                    <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
-                </v-list>
-            </v-navigation-drawer>
-            <v-main style="height: 250px"></v-main>
-        </v-layout>
+    <v-card max-width="300">
+        <v-list density="compact">
+            <v-list-subheader>REPORTS</v-list-subheader>
+            <v-list-item @click="chooseOperation(item.text)" v-for="(item, i) in items" :key="i" :value="item"
+                active-color="primary">
+                <template v-slot:prepend>
+                    <v-icon :icon="item.icon"></v-icon>
+                </template>
+                <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item>
+        </v-list>
     </v-card>
 </template>
 <script>
+import { useCommunicationStore } from "@/stores/communication";
 export default {
     name: "Sidebar",
     components: {},
     setup() {
-        return {};
+        const store = useCommunicationStore();
+        const chooseOperation = (component) => {
+            store.setChosenComponent(component);
+        };
+        let selectedItem = 0;
+        const items = [
+            { text: 'Create New Client', icon: 'mdi-plus' },
+            { text: 'All Clients', icon: 'mdi-account-multiple' },
+            { text: 'Upload New Video', icon: 'mdi-upload' },
+        ];
+        return { items, selectedItem, chooseOperation };
     },
 };
 </script>
+<style lang="scss">
+.sidebar-container {
+    flex-grow: 222;
+    display: flex;
+
+    & .v-navigation-drawer {
+        position: unset;
+    }
+}
+</style>
